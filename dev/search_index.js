c40be9990bb086b969e5268dc109e33e45a6fe36
Documenter.jl <documenter@juliadocs.github.io>
2023-08-26 07:20:22 +0000

    build based on 7fe0b34

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = CHomP","category":"page"},{"location":"#CHomP","page":"Home","title":"CHomP","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for CHomP.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [CHomP]","category":"page"}]
+}
